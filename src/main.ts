30b2154e0aedf18414b8f7cdd3a265f692322ea3
Aaron <aaron@cute.engineer>
2026-03-01 18:16:18 +1100

    attempting physics impl using matter

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import Matter from "matter-js";
 import { Application, Assets, Sprite } from "pixi.js";
 
 (async () => {
@@ -8,28 +9,87 @@ import { Application, Assets, Sprite } from "pixi.js";
   await app.init({ background: "#1099bb", resizeTo: window });
 
   // Append the application canvas to the document body
-  document.getElementById("pixi-container")!.appendChild(app.canvas);
+  const pixiContainer = document.getElementById("pixi-container")!;
+  pixiContainer.appendChild(app.canvas);
+
+  const engine = Matter.Engine.create();
+
+  const wallTop = Matter.Bodies.rectangle(
+    app.screen.width / 2,
+    0,
+    app.screen.width,
+    10,
+    {
+      isStatic: true,
+    },
+  );
+
+  const wallBottom = Matter.Bodies.rectangle(
+    app.screen.width / 2,
+    app.screen.height,
+    app.screen.width,
+    10,
+    {
+      isStatic: true,
+    },
+  );
+
+  const wallRight = Matter.Bodies.rectangle(
+    app.screen.width,
+    app.screen.height / 2,
+    10,
+    app.screen.height,
+    {
+      isStatic: true,
+    },
+  );
+
+  const wallLeft = Matter.Bodies.rectangle(
+    0,
+    app.screen.height / 2,
+    10,
+    app.screen.height,
+    {
+      isStatic: true,
+    },
+  );
+
+  Matter.World.add(engine.world, [wallTop, wallBottom, wallRight, wallLeft]);
+
+  const mouseConstraint = Matter.MouseConstraint.create(engine, {
+    mouse: Matter.Mouse.create(app.canvas),
+    constraint: {
+      render: { visible: true },
+    },
+  });
+
+  Matter.World.add(engine.world, mouseConstraint);
 
   // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const bunnyTexture = await Assets.load("/assets/bunny.png");
 
   // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  const bunnySprite = new Sprite(bunnyTexture);
+  bunnySprite.anchor.set(0.5);
+  bunnySprite.position.set(app.screen.width / 2, app.screen.height / 2);
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+  app.stage.addChild(bunnySprite);
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  const bunnyBody = Matter.Bodies.rectangle(
+    bunnySprite.x,
+    bunnySprite.y,
+    bunnySprite.width,
+    bunnySprite.height,
+    { restitution: 0.8 },
+  );
 
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
+  Matter.World.addBody(engine.world, bunnyBody);
 
   // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+  app.ticker.add(() => {
+    bunnySprite.position = bunnyBody.position;
+    bunnySprite.rotation = bunnyBody.angle;
   });
+
+  Matter.Runner.run(engine);
 })();
